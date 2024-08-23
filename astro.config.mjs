f7a863460fb3ccf5726238b314ef8434294ba685
Pol Cortés <polcortesg@gmail.com>
2024-08-23 16:33:27 +0200

    add @vercel/analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
